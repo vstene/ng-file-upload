07cf24c926849658fe6c000ba8dc04b5346d88ed
Vegard Stene <jvstene@gmail.com>
2018-12-10 10:11:11 +0100

    Build

--- dist/ng-file-upload-all.js
+++ dist/ng-file-upload-all.js
@@ -615,7 +615,7 @@ ngFileUpload.service('UploadBase', ['$http', '$q', '$timeout', function ($http, 
     if (promise['finally'] && promise['finally'] instanceof Function) {
       promise['finally'](function () {
         upload.promisesCount--;
-      });
+      }).then(angular.noop, angular.noop);
     }
     return promise;
   }
@@ -1041,8 +1041,8 @@ ngFileUpload.service('Upload', ['$parse', '$timeout', '$compile', '$q', 'UploadE
         if (validateAfterResize) {
           upload.validate(allNewFiles, keep ? prevValidFiles.length : 0, ngModel, attr, scope)
             .then(function (validationResult) {
-              valids = validationResult.validsFiles;
-              invalids = validationResult.invalidsFiles;
+              valids = validationResult.validFiles;
+              invalids = validationResult.invalidFiles;
               updateModel();
             });
         } else {
@@ -2096,14 +2096,15 @@ ngFileUpload.service('UploadValidate', ['UploadDataUrl', '$q', '$timeout', funct
 
         el.on('loadedmetadata', success);
         el.on('error', error);
+        
         var count = 0;
-
         function checkLoadError() {
+          count++;
           $timeout(function () {
             if (el[0].parentNode) {
               if (el[0].duration) {
                 success();
-              } else if (count > 10) {
+              } else if (count++ > 10) {
                 error();
               } else {
                 checkLoadError();
